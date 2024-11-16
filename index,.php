d4a5dba0abf7cec02dfce4918c3ea7f9407f8bca
Jaber Hossain Mozumder Opi <153591590+Jaberofficial@users.noreply.github.com>
2024-11-16 20:33:32 +0600

    php_function_loop

---
+++ index,.php
@@ -0,0 +1,164 @@
+<?php
+define("MESSAGE","Hello Php");
+const name = "john";
+//const name = "Doe";
+//echo MESSAGE;
+//echo name;
+
+/*echo __DIR__."<br>";
+echo __FILE__."<br>";
+echo __LINE__; */
+
+/*echo 2-5;
+//echo 8*5;
+// $x =5;
+// $x+= 7;
+// echo $x;*/
+
+/*
+$x ="Hello";
+$x .= "world";
+echo $x;*/
+/*
+$result = 12>5;
+if(true)
+{
+echo "this is true value";
+
+
+}*/
+
+/*
+//$result = 12<5;
+$result = 5-7;
+if($result)
+{
+echo "this is true value";
+}
+else if($result<0){
+    echo "this is a negetive number ";
+
+}
+
+else{
+echo "this is false value";
+
+}*/
+
+/*$day =10;
+switch(day){
+case 0:
+    echo "sunday";
+    break;
+    case 1:
+        echo "monday;
+        break;
+    case 2:
+        echo "tuesday";
+        break;
+    case 3:
+        echo "wednesday";
+        break;
+    case 4:
+        echo "thursday";
+        break;
+    case 5:
+        echo "friday";
+        break;
+    case 6:
+        echo "saturday";
+        break;
+ default:
+        echo "Invalid date";
+
+}*/
+
+
+/*$result = (5>4)?"True":"False";
+echo $result;
+(ternary operator)*/
+
+/*$result = (5>4)?"True":"False";
+// echo $result;
+// $num = 10;
+// if($num%2==0){
+// echo "this is even number<br>";
+// if($num>10){
+// echo "this value is also greater than 10";
+
+// }else{
+//     echo "less then 10";
+// }
+// }
+
+
+// $num = 7;
+// if($num%2==0){
+// echo "this is even number<br>";
+// if($num>10){
+// echo "this value is also greater than 10";
+
+// }else{
+//     echo "less then 10";
+// }
+// }else{
+//     echo "this ois odd number";
+// }
+*/
+
+
+/* 1. Take a username and password
+2.username match
+3.password match
+(assignment)*/
+
+ //
+ $num =12;
+// while($num<=10){
+//     echo "this is line no: $num<br>";
+//     $num++;
+// }
+
+
+/*do{
+    echo"this is line no: $num<br>";
+$num++;
+
+}while($num<=10);
+*/
+
+/*for($num = 1;$num <+10;$num++){
+    echo "this is number : $num<br>";
+}*/
+
+
+
+/*          function welcome_message(){
+// echo  "hello welcome,we are learning php";
+
+//           }      
+// //           welcome_message();                                                                                                                                                                                                 
+//           function welcome_message($name ="änonymus" ){
+//             echo  "hello $name welcome,we are learning php";
+            
+//                       }      
+//                       welcome_message();                                                                                                                          
+
+//           function welcome_message($name ){
+//             echo  "hello $name welcome,we are learning php";
+            
+//                       }      
+//                       welcome_message(johom);     */                                         
+
+/*var_dump(welcome_message);
+//welcome_message("Doe");
+// $cars = array("car1","car2","car3");
+$cars = ["car1","car2","car3"];
+// print_r($cars[1]);
+$cars[0]="car";
+print_r($cars);
+*/
+
+
+
+?>
