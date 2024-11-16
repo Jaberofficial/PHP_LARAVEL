0d0237bdd7eed2b938569d06acbafab4d0af11e5
Jaber Hossain Mozumder Opi <153591590+Jaberofficial@users.noreply.github.com>
2024-11-16 20:32:10 +0600

    php_basic

---
+++ index.php
@@ -0,0 +1,108 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3> this is php content</h3>  
+
+<p>
+<?php
+echo "This is php element";
+
+
+
+?>
+
+</p>
+
+
+
+</body>
+</html> -->
+
+
+<?php
+//this is single line comment
+/*this is multiline <comment></comment> */
+/**php variables : x=5;4578f;
+ 
+  */
+
+
+/*$num1 = 11;
+$num = 12;
+//echo $num;
+//echo "the number is $num";
+//print("$num"." $num1");
+print("the number is $num1 $num"
+);
+//echo "the number is $num1 $num";*/
+
+/* $num = 'hello';
+//var_dump($num);
+printf($num); */
+
+/*$number =10;
+$decimal =10.2;
+$decision =true;
+$array =[1,2,3,4];
+echo "<pre>";
+var_dump($number);
+echo "<br>";
+var_dump($decimal);
+echo "<br>";
+var_dump($decision);
+echo "<br>";
+var_dump($array);
+echo "</pre>";
+*/
+
+
+    /*  //$text = "Hello  php";
+      $text = "Hello,php,version8.23";
+      $email = "JABER@GMAIL.COM";
+      $pin = "    83346    ";
+      //echo strlen($text);
+      //echo str_word_count($text,);
+      //echo strpos($text, "php");
+      //echo strtoUpper($text);
+      //echo strtoLower($email);
+      //echo str_replace("php","world",$text);
+      //echo strrev($text);
+      //echo strlen($pin);
+      //echo strlen(trim($pin));
+//print_r(explode(",",$text)); */
+
+// $name ="jaber";
+// $age = 20;
+// $text ='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita earum porro corporis animi illum quo delectus tempora? Ut quidem harum id facere non sequi? Ullam dolores blanditiis ea laboriosam corrupti.';
+
+//echo "our new student name is $name.His age is : $age";
+//echo "our new student name is $name"."His age is : $age";
+
+//echo substr($text,5,30);
+//////////echo PHP_INT_MAX;
+
+//$num = 12.12;
+//echo is_float($num);
+//var_dump($num);
+
+//$num = asin(8);
+//var_dump($num);
+//var_dump(25+"25");
+//var_dump("25"+25);
+
+//$before =25;
+//var_dump($before);
+//$after = (string)25;
+//var_dump($after);
+
+//var_dump(pi());
+//$num =10;
+//echo abs($num);
+
+
+?>
